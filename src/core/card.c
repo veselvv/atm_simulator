ec07fd7e9104271b8d81d825c293cb4a3ea4b0bc
Vasia <veselvv12@yandex.ru>
2025-10-19 02:25:52 +0300

    add define_card/generate_cvv/generate_card_num/ in card.c & card.h

---
+++ src/core/card.c
@@ -0,0 +1,49 @@
+#include "card.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <time.h> 
+#define True 1
+#define False 0
+
+
+
+Card *create_card(const char *pin,
+    const char *name, double balance, const char *login, const char *phone_number){
+    //memmory for NEw card
+    Card *new_card = malloc(sizeof(Card));
+
+    //Identify Person data
+    create_card_number(new_card->card_number);
+    strcpy(new_card->holder_name, name);
+    strcpy(new_card->login, login);
+    strcpy(new_card->phone_number, phone_number);
+
+    //safety
+    strcpy(new_card->pin, pin);
+    new_card->is_blocked=False;
+    new_card->pin_atempts=3;
+
+    //Balance
+    new_card->balance = balance;
+
+    //OTHER
+    generate_random_cvv(new_card->cvv);
+    new_card->created_date = time(NULL);
+    
+    return new_card;
+}
+
+
+void generate_random_cvv(char *cvv_buffer){
+    int new_cvv = (rand()%999)+100;
+    sprintf(cvv_buffer, "%03d",new_cvv);
+}
+
+void create_card_number(char *card_number_buffer){
+    card_number_buffer[0]='4';
+    for (int i =1;i<16;i++){
+        card_number_buffer[i] = '0' + rand()%10;
+    }
+    card_number_buffer[16] = '\0';
+}
---
+++ src/core/card.h
@@ -0,0 +1,30 @@
+#ifndef CARD_H
+#define CARD_H
+#include <time.h> 
+typedef struct Card{
+    //Идентификация пользователя
+    char card_number[17];
+    char holder_name[51];
+
+    //Безопасность
+    char pin[5];
+    int is_blocked;
+    int pin_atempts;
+
+    //Баланс
+    double balance;
+
+    //Доп информация
+    char expiry_date[6];
+    char cvv[4];
+    char phone_number[12];
+    char login[31];
+    time_t created_date;
+
+}Card;
+
+Card *create_card(const char *pin, const char *name, double balance, const char *login, const char *phone_number);
+void generate_random_cvv(char *cvv_buffer);
+void create_card_number(char *card_number_buffer);
+
+#endif
